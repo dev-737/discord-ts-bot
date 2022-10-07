caac41bb7f6ebabb3ed30ec94a9057d4061f666f
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-10-07 09:06:06 +0530

    feat: Upated ping command

--- src/commands/Main/ping.ts
+++ src/commands/Main/ping.ts
@@ -3,5 +3,5 @@ import { CommandInteraction } from 'discord.js'
 
 export const data = new SlashCommandBuilder().setName('ping').setDescription('Responds with pong!')
 export function execute(interaction: CommandInteraction) {
-return interaction.reply('Pong!')
+    return interaction.reply(`Pong! 🏓Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(interaction.client.ws.ping)}ms`)
 }
